7ad8a0dc56fabe3cab6c6eeb659205352600d112
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2023-03-26 12:46:47 -0400

    Clarify comment in CLI

--- cmd/openai/main.go
+++ cmd/openai/main.go
@@ -96,7 +96,7 @@ func main() {
 
 	// Wait maxiumum of 120 seconds for a response, which provides
 	// a lot of time for the API to respond, but it should a matter
-	// of seconds, not minutes.
+	// of seconds, not minutes for most models / requests.
 	ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
 	defer cancel()
 
