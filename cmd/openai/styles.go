7d9813d52310bbcf0259ec359d7d84104e1d3a3b
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2025-07-13 21:13:27 -0400

    Remove unused styles

--- cmd/openai/styles.go
+++ cmd/openai/styles.go
@@ -6,9 +6,6 @@ var (
 	styleBold    = lipgloss.NewStyle().Bold(true)
 	styleFaint   = lipgloss.NewStyle().Faint(true)
 	styleWarning = lipgloss.NewStyle().Foreground(lipgloss.Color("9"))
-	numberColor  = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("69"))
 	styleInfo    = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#57C7FF")) // Blue
-	styleSuccess = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#5AFFA"))  // Green
-	styleError   = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#FF5A5F")) // Red
 	stylePath    = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFD75F"))
 )
